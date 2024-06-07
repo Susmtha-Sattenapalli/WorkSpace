671c553ed4d8c74a483bc3f7c95f1bb779a6e61b
Susmtha-Sattenapalli <166799268+Susmtha-Sattenapalli@users.noreply.github.com>
2024-06-07 13:37:06 +0530

    Add files via upload

---
+++ quizapp/quizapp/src/main/java/com/susmitha/quizapp/model/Quiz.java
@@ -0,0 +1,22 @@
+package com.susmitha.quizapp.model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+
+@Data
+@Entity
+public class Quiz {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String title;
+
+    @ManyToMany
+    private List<Question> questions;
+}
