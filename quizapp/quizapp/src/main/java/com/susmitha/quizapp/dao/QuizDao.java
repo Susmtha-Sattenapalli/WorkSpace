33bcf9ea17c0bf3d3b93cafb678a4df06b10394a
Susmtha-Sattenapalli <166799268+Susmtha-Sattenapalli@users.noreply.github.com>
2024-06-07 13:35:58 +0530

    Delete quizapp/quizapp directory

--- quizapp/quizapp/src/main/java/com/susmitha/quizapp/dao/QuizDao.java
+++
@@ -1,8 +0,0 @@
-package com.susmitha.quizapp.dao;
-
-import com.susmitha.quizapp.model.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizDao extends JpaRepository<Quiz, Integer> {
-
-}
